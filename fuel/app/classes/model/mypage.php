d3c990e775190a820899445d472e5e97087bb2d5
Yohei Hino <m5181146@gmail.com>
2014-09-17 20:37:00 +0900

    MypageのControllerとModel作成

--- fuel/app/classes/model/mypage.php
+++ fuel/app/classes/model/mypage.php
@@ -1,6 +1,11 @@
 <?php
 
-class Mypage()
-{
 
+Class Model_Mypage extends \Model{	
+	public static function action_select($user_id){
+		 //データベース接続
+		$data = DB::query("SELECT * FROM users WHERE id = $user_id  ")-> execute() -> as_array();
+   		//var_dump($data);
+		return $data[0];
+	}
 }
---
+++ fuel/app/tmp/Smarty/templates_c/add80661a3aee0646078a927cea0d1ebb043a7d9.file.mypage.tpl.php
@@ -0,0 +1,28 @@
+<?php /* Smarty version Smarty-3.1.19-dev, created on 2014-09-17 20:35:09
+         compiled from "/var/www/fuelphp/fuel/app/views/sukima/mypage.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:1560131080541971a0b7eb90-76409644%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'add80661a3aee0646078a927cea0d1ebb043a7d9' => 
+    array (
+      0 => '/var/www/fuelphp/fuel/app/views/sukima/mypage.tpl',
+      1 => 1410953707,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1560131080541971a0b7eb90-76409644',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.19-dev',
+  'unifunc' => 'content_541971a0b87e77_66690609',
+  'variables' => 
+  array (
+    'name' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_541971a0b87e77_66690609')) {function content_541971a0b87e77_66690609($_smarty_tpl) {?><?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+
+<?php }} ?>
