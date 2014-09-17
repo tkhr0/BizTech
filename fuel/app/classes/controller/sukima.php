5150df64ff32fe5ee5505d6dfbab23e7e5d841b0
Karasawa Takahiro <tkhr.karasawa@gmail.com>
2014-09-17 21:24:34 +0900

    create Controller files create Model files  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   fuel/app/classes/controller/sukima.php
    	new file:   fuel/app/classes/model/achieve.php
    	new file:   fuel/app/classes/model/cheer.php
    	new file:   fuel/app/classes/model/follower.php
    	new file:   fuel/app/classes/model/hack_end.php
    	new file:   fuel/app/classes/model/hack_start.php
    	new file:   fuel/app/classes/model/index.php
    	new file:   fuel/app/classes/model/mypage.php
    	new file:   fuel/app/classes/model/new.php
    	new file:   fuel/app/classes/model/timeline.php

---
+++ fuel/app/classes/controller/sukima.php
@@ -0,0 +1,60 @@
+<?php
+class Controller_Sukima extends Controller
+{
+        public function action_index()
+        {
+
+
+        }
+
+        public function action_mypage()
+        {
+
+        }
+
+        public function action_timeline()
+        {
+
+        }
+
+        public function post_new()
+        {
+
+        }
+
+        public function post_hack_start()
+        {
+        
+        }
+
+        public function post_hack_end()
+        {
+
+        }
+
+        public function post_achieve()
+        {
+
+        }
+
+        public function post_follower()
+        {
+
+        }
+
+        public function post_cheer()
+        {
+
+        }
+
+	/**
+	 * The 404 action for the application.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_404()
+	{
+		return Response::forge(Presenter::forge('welcome/404'), 404);
+	}
+}
