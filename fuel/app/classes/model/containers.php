6c35ccb91c11789af2eb81f5b1441142af2b4720
Karasawa Takahiro <tkhr.karasawa@gmail.com>
2014-09-19 03:30:03 +0900

    Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   classes/model/containers.php

--- fuel/app/classes/model/containers.php
+++ fuel/app/classes/model/containers.php
@@ -1,6 +1,36 @@
 <?php
-namespace Model;
 
-class Containers extends \Model {
+use DB;
+
+class Model_Containers extends \Model
+{
+        public static function insert($goal_id, $cheered=0, $status=1)
+        {
+                $query = DB::insert('containers')->set(array(
+                        'goal_id' => $goal_id,
+                        'cheered' => $cheered,
+                        'status'  => $status,
+                )->execute();
+
+                return $query;
+        }
+
+        public static function get_goal_id($id)
+        {
+                $query = DB::select('goal_id')->from('containers')->where('id', '=', $id)->execute();
+                return $query->as_array()[0]['goal_id'];
+        }
+
+        public static function cheered($id)
+        {
+                $query = DB::select('cheered')->from('containers')->where('id', '=', $id)->execute();
+                return $query->as_array()[0]['cheered'];
+        }
+
+        public static function status($id)
+        {
+                $query = DB::select('status')->from('containers')->where('id', '=', $id)->execute();
+                return $query->as_array()[0]['status'];
+        }
 
 }
