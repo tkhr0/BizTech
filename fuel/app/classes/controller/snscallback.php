73b995a88a3cb7644b3025007f229af1133e08e1
Yohei Hino <m5181146@gmail.com>
2014-09-21 01:15:15 +0900

    followuserを20人から全員分とるように変更

--- fuel/app/classes/controller/snscallback.php
+++ fuel/app/classes/controller/snscallback.php
@@ -23,7 +23,7 @@ class Controller_Snscallback extends Controller{
      $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
      
      //ユーザ情報取得　デフォルトでは20件   
-     $user_info = $connection->get('account/verify_credentials');
+     //$user_info = $connection->get('account/verify_credentials');
      
    
      //print $user_info->name;
@@ -34,10 +34,31 @@ class Controller_Snscallback extends Controller{
      //print "<br>";
      //フレンド情報の取得
      
+
+
+     $cursor = -1;
+     
+
+       
+    do{   
+        $friends = $connection->get('friends/list',array('count'=>'200', 'cursor'=> $cursor)); 
+        foreach($friends->users as $friend){
+            $tmp = array(
+                "name"        => $friend->name, 
+                "screen_name" => $friend->screen_name,
+                "profile_img" => $friend->profile_image_url,
+            );
+
+        $followerprop_ary[] = $tmp;
+      }
+      $cursor = $friends->next_cursor_str;
+     }while($cursor != "0");
+     
      //$friends = $connection->get('friends/list');
     
-     Model_users::set_profile($user_info->screen_name, $user_info->name, $user_info->profile_image_url_https);    
-    
+     //Model_users::set_profile($user_info->screen_name, $user_info->name, $user_info->profile_image_url_https);    
+     var_dump($followerprop_ary);
+     exit;
     // foreach($friends->users as $obj){
     //	print $obj->screen_name;
     //	print "<br>";
