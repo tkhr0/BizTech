ff1315263b8cbb90cad04e01b5a96632db71bc96
Imamori Daichi <aidy.91614@gmail.com>
2014-09-21 23:38:21 +0900

    routes.phpの修正

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
+	'_root_'  => 'sukima/index',  // The default route
 	'_404_'   => 'sukima/404.html',    // The main 404 route
 	
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
