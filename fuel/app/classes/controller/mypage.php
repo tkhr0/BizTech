68191de8637b5d26d1769d92a1979ed36003a832
Imamori Daichi <aidy.91614@gmail.com>
2014-09-21 23:48:28 +0900

    mypage controllerを削除

--- fuel/app/classes/controller/mypage.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Controller_Mypage extends Controller{
-   public function action_index()
-    {
-     $data = Model_Mypage::action_select(2);
-     return Response::forge(View_Smarty::forge('sukima/mypage.tpl', $data));
-    }
-}
